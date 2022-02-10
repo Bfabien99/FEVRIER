f159e89569191763b8b76b6fe330c7712106ed4a
Bfabien99 <fabienbrou99@gmail.com>
2022-02-10 10:02:51 +0000

    first commit

---
+++ Social/assets/script/model.js
@@ -0,0 +1,15 @@
+let radio = document.querySelectorAll('input[type="radio"]');
+
+    radio[0].addEventListener('click',function(event){    
+        
+        radio[0].parentElement.classList.toggle("red")
+        radio[1].parentElement.classList.remove("red")
+        
+    })
+
+    radio[1].addEventListener('click',function(event){    
+        
+        radio[1].parentElement.classList.toggle("red")
+        radio[0].parentElement.classList.remove("red")
+        
+    })
